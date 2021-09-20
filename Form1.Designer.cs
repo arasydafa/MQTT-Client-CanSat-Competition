e9af2f03328d310eba15252b79f2dbbc33946005
Arasy Dafa Sulistya Kurniawan <arasy.dafa@gmail.com>
2021-09-20 13:31:19 +0700

    Upload to github

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace MQTT_Client_CanSat_Competition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.publishButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // publishButton
+            // 
+            this.publishButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(53)))), ((int)(((byte)(102)))));
+            this.publishButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.publishButton.Font = new System.Drawing.Font("Roboto Condensed", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.publishButton.ForeColor = System.Drawing.Color.White;
+            this.publishButton.Location = new System.Drawing.Point(224, 71);
+            this.publishButton.Name = "publishButton";
+            this.publishButton.Size = new System.Drawing.Size(330, 271);
+            this.publishButton.TabIndex = 1;
+            this.publishButton.Text = "PUBLISH";
+            this.publishButton.UseVisualStyleBackColor = false;
+            this.publishButton.Click += new System.EventHandler(this.publishButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(160)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.publishButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button publishButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MQTT_Client_CanSat_Competition
+{
+    public partial class Form1 : Form
+    {
+        string state;
+        string data;
+        string bin = Application.StartupPath;
+
+        string topic = "teams/9999";
+        string username = "9999";
+        string password = "teams9999";
+        MqttClient client;
+        bool mqtt = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void publishButton_Click(object sender, EventArgs e)
+        {
+            while (mqtt == true)
+            {
+                if (state != "LANDED")
+                {
+                    if (!String.IsNullOrEmpty(data))
+                    {
+
+                        Application.DoEvents();
+
+                        try
+                        {
+
+                            var pathFile = bin + "\\FLIGHT_9999_C.csv";
+                            var filename = File.Open(pathFile, FileMode.Open, (FileAccess)FileIOPermissionAccess.Read, FileShare.ReadWrite);
+                            StreamReader reader = new StreamReader(filename);
+
+                            while (!reader.EndOfStream)
+                            {
+                                string line = reader.ReadLine();
+
+                                if (!String.IsNullOrWhiteSpace(line))
+                                {
+                                    string[] values = line.Split(',');
+
+                                    if (values.Length > 1)
+                                    {
+                                        if (values[3] == "C")
+                                        {
+                                            Application.DoEvents();
+                                            Thread.Sleep(1000);
+                                        }
+                                    }
+                                }
+                                client.Publish(topic, Encoding.UTF8.GetBytes(line), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, false);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            return;
+                        }
+                    }
+                }
+                else if (state == "LANDED")
+                {
+                    mqtt = false;
+                    client.Disconnect();
+                }
+            }
+        }
+    }
+}
